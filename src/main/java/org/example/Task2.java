fa105c09d2208332fdbcc76bf56de82ebc4825be
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2026-04-28 22:09:36 +0300

    first commit

---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Traverse_from_Parent_to_Child {
+
+    private WebDriver driver;
+
+    public main()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
