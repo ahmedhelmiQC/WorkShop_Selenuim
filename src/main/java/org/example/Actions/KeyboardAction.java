b060657115e68f4a76989b769096e9bfb3ffea69
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2026-07-21 00:28:34 +0300

    KeyboardAction

--- src/main/java/org/example/Actions/KeyboardAction.java
+++ src/main/java/org/example/Actions/KeyboardAction.java
@@ -1,4 +1,42 @@
 package org.example.Actions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class KeyboardAction {
+
+    protected static WebDriver driver;
+    protected static By keys = By.id("target");
+
+    protected static Actions actions;
+
+    public static void main(String[] args) {
+        setup();
+        //keypress();
+
+    }
+
+    public static void setup() {
+        driver = new ChromeDriver();
+        //driver.get("https://the-internet.herokuapp.com/key_presses");
+        driver.get("https://the-internet.herokuapp.com");
+
+        actions = new Actions(driver);
+
+    }
+
+    public static WebElement findwebelement(By by) {
+        return driver.findElement(by);
+    }
+
+    public static void keypress() {
+        // actions.keyDown(Keys.ENTER).perform();
+        actions.keyDown(Keys.SHIFT).sendKeys(findwebelement(keys), "test").perform();
+    }
+
+
 }
--- src/main/java/org/example/Actions/MousAction.java
+++ src/main/java/org/example/Actions/MousAction.java
@@ -12,18 +12,29 @@ public class MousAction {
     public static By user1 = By.xpath("//div[@class='example']/div[1]");
     protected static WebDriver driver;
     protected static By rightClick = By.id("hot-spot");
+
+    protected static By sourceelement = By.id("column-a");
+    protected static By destinationelement = By.id("column-b");
+
+    protected static By scrollelement = By.partialLinkText("Status Codes");
+
     protected static Actions actions;
 
     public static void main(String[] args) {
         setup();
         // contextClick();
-        hover();
+        //hover();
+        //dragdrop();
+        // clickandhold();
+        scrolling();
     }
 
     public static void setup() {
         driver = new ChromeDriver();
         // driver.get("https://the-internet.herokuapp.com/context_menu");
-        driver.get("https://the-internet.herokuapp.com/hovers");
+        // driver.get("https://the-internet.herokuapp.com/hovers");
+        //driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+        driver.get("https://the-internet.herokuapp.com"); // scroll
         actions = new Actions(driver);
 
     }
@@ -32,12 +43,33 @@ public class MousAction {
         return driver.findElement(by);
     }
 
+    /// ////            Context Click / Right Click
     public static void contextClick() {
-
         actions.contextClick(findwebelement(rightClick)).perform();
     }
 
+    /// //////         Hover
     public static void hover() {
         actions.moveToElement(findwebelement(user1)).perform();
+    }
+
+    /// //          Drag and Drop
+    public static void dragdrop() {
+        WebElement source = findwebelement(sourceelement);
+        WebElement destination = findwebelement(destinationelement);
+        actions.dragAndDrop(source, destination).perform();
+    }
+
+    ///         Click and Hold
+    public static void clickandhold() {
+        WebElement source = findwebelement(sourceelement);
+        WebElement destination = findwebelement(destinationelement);
+        actions.clickAndHold(destination).moveToElement(source).perform(); // Click and Hold
+        actions.clickAndHold(destination).moveToElement(source).release().perform(); // Click and Hold , drop
+    }
+
+    ///       Scrolling
+    public static void scrolling() {
+        actions.scrollToElement(findwebelement(scrollelement)).perform();
     }
 }
